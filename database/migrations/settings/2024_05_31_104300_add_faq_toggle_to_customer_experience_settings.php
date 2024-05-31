833e8cc6be93670b5340470b28a988f736144633
Kofi Ramos <>
2024-05-31 21:29:32 +0000

    Add migration for FAQ enabling

---
+++ database/migrations/settings/2024_05_31_104300_add_faq_toggle_to_customer_experience_settings.php
@@ -0,0 +1,16 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('customer-experience.faq_enabled', false);
+    }
+
+    public function down()
+    {
+        $this->migrator->delete('customer-experience.faq_enabled');
+    }
+}
