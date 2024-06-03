49bdc543a003a93f7a91a71b8fd5a6fccce71f5d
OniiCoder <oniicoder@gmail.com>
2024-06-03 14:58:47 +0000

    Add advancedformat

--- resources/views/components/cx-scripts.blade.php
+++ resources/views/components/cx-scripts.blade.php
@@ -4,5 +4,6 @@
         <script src="https://cdn.jsdelivr.net/npm/dayjs@1/plugin/utc.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/dayjs@1/plugin/timezone.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/dayjs@1/plugin/customParseFormat.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.7/plugin/advancedFormat.min.js"></script>
     @endpush
 @endif
