ada7a28c83f7f1188f5ec7bf19bdf8cb4182e885
Laura Tonning <tonning@gmail.com>
2024-07-03 14:29:24 +0200

    Use app asset components (#3)

--- resources/views/components/cx-scripts.blade.php
+++
@@ -1,5 +0,0 @@
-@if(app(\Astrogoat\CustomerExperience\Settings\CustomerExperienceSettings::class)->isEnabled())
-    @push('footer')
-        <script src="{{ asset('vendor/customer-experience/js/customer-experience.js') }}"></script>
-    @endpush
-@endif
