8bc87b63192bf7a634b1291d439cb241e596dfe9
Laura Tonning <tonning@gmail.com>
2024-10-08 14:22:39 +0200

    Fix wrong route

--- routes/backend.php
+++ routes/backend.php
@@ -1,23 +1,18 @@
 <?php
 
-// Here you can add routes for the backend.
-    use Astrogoat\CustomerExperience\Http\Livewire\Models\FaqForm;
-    use Astrogoat\CustomerExperience\Http\Livewire\Models\FaqIndex;
-    use Illuminate\Support\Facades\Route;
+use Astrogoat\CustomerExperience\Http\Livewire\Models\FaqForm;
+use Astrogoat\CustomerExperience\Http\Livewire\Models\FaqIndex;
+use Illuminate\Support\Facades\Route;
 
-    Route::group([
-        'as' => 'customer-experience.faqs.',
-        'prefix' => 'customer-experience/faqs/'
-    ], function () {
-        Route::get('/', FaqIndex::class)->name('index');
-        Route::get('/create', FaqForm::class)->name('create');
-        Route::get('/{faq}/edit', FaqForm::class)->name('edit');
-    });
+Route::group([
+    'as' => 'customer-experience.faqs.',
+    'prefix' => 'customer-experience/faqs/'
+], function () {
+    Route::get('/', FaqIndex::class)->name('index');
+    Route::get('/create', FaqForm::class)->name('create');
+    Route::get('/{faq}/edit', FaqForm::class)->name('edit');
+});
 
-    Route::get('/admin/apps/customer-experience',function(){
-        return redirect('/admin/apps/customer-experience');
-    })->name('customer-experience.index');
-
-    Route::get('/admin/apps/',function(){
-        return redirect('/admin/apps');
-    })->name('apps.index');
+Route::get('apps/customer-experience',function(){
+    return redirect('/admin/apps/customer-experience');
+})->name('customer-experience.index');
