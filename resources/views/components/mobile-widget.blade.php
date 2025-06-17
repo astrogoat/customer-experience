c15b45a39424f7c1e6901587e6048614251dd27b
Travis Ray <tripp.machine@gmail.com>
2025-06-17 06:06:09 -0400

    DTCE-2949: CX Widget updates (#13)
    Co-authored-by: Laura Tonning <tonning@gmail.com>

--- resources/views/components/mobile-widget.blade.php
+++ resources/views/components/mobile-widget.blade.php
@@ -32,7 +32,7 @@
 
     <div
         data-area="cx"
-        class="flex flex-col space-y-4"
+        class="flex flex-col space-y-4 {{ $this->css('cxMobileBackground') }}"
         x-data="{
             clientTimezone: '',
             clientTimezoneAbbreviation: '',
@@ -53,8 +53,8 @@
         @if($chatEnabled)
           <div class="flex items-center justify-between {{ $this->css('cxMobileButtonContainer') }}">
               <div class="flex items-center space-x-2 {{ $this->css('cxMobileButtonCta') }}">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                      <path d="M9.87891 7.8441C11.0505 6.81897 12.95 6.81897 14.1215 7.8441C15.2931 8.86923 15.2931 10.5313 14.1215 11.5564C13.9176 11.7348 13.6917 11.8822 13.4513 11.9985C12.7056 12.3594 12.0002 12.9968 12.0002 13.8253V14.5753M21 12.3253C21 17.2958 16.9706 21.3253 12 21.3253C7.02944 21.3253 3 17.2958 3 12.3253C3 7.35469 7.02944 3.32526 12 3.32526C16.9706 3.32526 21 7.35469 21 12.3253ZM12 17.5753H12.0075V17.5828H12V17.5753Z" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  <svg class="{{ $this->css('cxQuestionMarkIcon') }}" xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+                      <path d="M9.87891 7.8441C11.0505 6.81897 12.95 6.81897 14.1215 7.8441C15.2931 8.86923 15.2931 10.5313 14.1215 11.5564C13.9176 11.7348 13.6917 11.8822 13.4513 11.9985C12.7056 12.3594 12.0002 12.9968 12.0002 13.8253V14.5753M21 12.3253C21 17.2958 16.9706 21.3253 12 21.3253C7.02944 21.3253 3 17.2958 3 12.3253C3 7.35469 7.02944 3.32526 12 3.32526C16.9706 3.32526 21 7.35469 21 12.3253ZM12 17.5753H12.0075V17.5828H12V17.5753Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
                   <span>Have a question?</span>
               </div>
@@ -62,7 +62,7 @@
                   data-area="chat-now"
                   x-on:click="{{ $settings->chat_button_action }}"
                   type="button"
-                  class="cx-transition-colors cx-ease-in-out cx-duration cx-py-2 {{ $this->css('cxMobileButton') }}"
+                  class="cx-transition-colors cx-ease-in-out cx-duration {{ $this->css('cxMobileButton') }}"
                   aria-label="chat-now"
                   {{ $chatIsAvailable ? '' : 'disabled' }}
               >
